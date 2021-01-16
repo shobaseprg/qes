5fc5a5dbf7282315725c6902acd7b4772b355c41
shobaseprg <pgymlsho19128@gmail.com>
2021-01-16 17:16:11 +0900

    name_link

--- src/laravelapp/app/Http/Controllers/respondentController.php
+++ src/laravelapp/app/Http/Controllers/respondentController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Respondent;
+
 
 class respondentController extends Controller
 {
     public function index()
     {
-        return view('respondent.v_index');
+        $respondents = Respondent::all();
+        return view('respondent.v_index', compact('respondents'));
     }
 }
--- src/laravelapp/resources/views/respondent/v_index.blade.php
+++ src/laravelapp/resources/views/respondent/v_index.blade.php
@@ -1,1 +1,4 @@
-itiran
+@foreach($respondents as $respondent)
+<a href="/show/{{$respondent->id}}">名前: {{$respondent->name}}</a>
+<p>作成日: {{$respondent->created_at}}</p>
+@endforeach
