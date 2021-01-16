a67a80bc745d53d69da533a5db5a56287b5391e5
shobaseprg <pgymlsho19128@gmail.com>
2021-01-16 16:50:21 +0900

    seedr_three_respondent

--- src/laravelapp/database/seeders/respondentSeeder.php
+++ src/laravelapp/database/seeders/respondentSeeder.php
@@ -14,12 +14,35 @@ class respondentSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('respondents')->insert([
-            'name' => 'やまだ  たろう',
-            'age' => 20,
-            'sex' => 1,
-            'kind' => 1,
-            'other' => '特に無し'
-        ]);
+        DB::table('respondents')->insert(
+            [
+                'name' => 'やまだ  たろう',
+                'age' => 20,
+                'sex' => 1,
+                'kind' => 1,
+                'other' => '特に無し',
+                'created_at' => date('Y-m-d H:i:s'),
+            ]
+        );
+        DB::table('respondents')->insert(
+            [
+                'name' => 'すずき  じろう',
+                'age' => 30,
+                'sex' => 1,
+                'kind' => 2,
+                'other' => '角部屋希望',
+                'created_at' => date('Y-m-d H:i:s'),
+            ]
+        );
+        DB::table('respondents')->insert(
+            [
+                'name' => 'たなか  はなこ',
+                'age' => 40,
+                'sex' => 2,
+                'kind' => 3,
+                'other' => '最上階希望',
+                'created_at' => date('Y-m-d H:i:s'),
+            ]
+        );
     }
 }
