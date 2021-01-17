0818880189778f61e05d5fe6ee60b759c2a0fc6c
shobaseprg <pgymlsho19128@gmail.com>
2021-01-17 10:47:01 +0900

    time_display_fix

--- src/laravelapp/resources/views/respondent/v_index.blade.php
+++ src/laravelapp/resources/views/respondent/v_index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.base')
 
 @section('content')
-
-<div>
-    <a href="/create">回答する<a>
-</div>
+<ul class="nav">
+    <li class="nav-item">
+        <a class="nav-link active" href="/create">回答する</a>
+    </li>
+</ul>
 
 @foreach($respondents as $respondent)
-<a href="/show/{{$respondent->id}}">名前: {{$respondent->name}}</a>
-<p>作成日: {{$respondent->created_at}}</p>
+<a href="/show/{{$respondent->id}}">{{$respondent->name}}( {{$respondent->created_at->format('Y年n月j日')}})</a></br>
 @endforeach
 
 
